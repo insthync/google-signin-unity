bb3e29c7c8c0052b22d7b3db458accb6294c07c8
insthync <ittipon.bay@gmail.com>
2024-10-22 15:15:45 +0700

    fix: missing namespace

--- GoogleSignIn/GoogleSignIn.cs
+++ GoogleSignIn/GoogleSignIn.cs
@@ -20,6 +20,7 @@ namespace Google
     using System.Runtime.Serialization;
     using System.Threading.Tasks;
     using Google.Impl;
+    using UnityEngine;
 
     /// <summary>
     /// Google sign in API.
